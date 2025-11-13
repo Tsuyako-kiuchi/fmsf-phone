e41389007220e01e304f5a6906a7012a81c2533a
Tsuyako-kiuchi <isozaki@kiuchi.co.jp>
2025-11-13 22:13:57 +0900

    Delete functions/logout.js

--- functions/logout.js
+++
@@ -1,7 +0,0 @@
-export const onRequestGet = async () => {
-  const headers = new Headers({
-    'Set-Cookie': 'pin=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0',
-    'Location': '/login',
-  });
-  return new Response(null, { status: 302, headers });
-};
